ebe1ad276febc8b6043b736646068bd82b4f827a
Serumz <65189904+Serumz@users.noreply.github.com>
2021-02-21 15:36:47 +0000

    Update server.js

--- server.js
+++ server.js
@@ -25,3 +25,8 @@ app.use('/users', usersRouter);
 // app.listen(port, () => {
 //     console.log('Server is running on port: '+ port);
 // });
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
