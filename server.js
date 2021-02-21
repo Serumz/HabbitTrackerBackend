afca0750ae914b818591c1b0ae15df3b2452d19d
Serumz <65189904+Serumz@users.noreply.github.com>
2021-02-21 15:31:13 +0000

    Removed Port assignment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.port || 5000;
+// const port = process.env.port || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +22,6 @@ const usersRouter = require('./routes/user');
 app.use('/habbits', habbitsRouter);
 app.use('/users', usersRouter);
 
-app.listen(port, () => {
-    console.log('Server is running on port: '+ port);
-});
+// app.listen(port, () => {
+//     console.log('Server is running on port: '+ port);
+// });
